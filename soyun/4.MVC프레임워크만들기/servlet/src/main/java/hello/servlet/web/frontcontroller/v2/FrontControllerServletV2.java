d04a9a0081fa11e45a93bd67d41475c495a83d84
sypark <soun997@gmail.com>
2023-04-27 00:00:08 +0900

    practice: v2 - view 분리

---
+++ soyun/4.MVC프레임워크만들기/servlet/src/main/java/hello/servlet/web/frontcontroller/v2/FrontControllerServletV2.java
@@ -0,0 +1,44 @@
+package hello.servlet.web.frontcontroller.v2;
+
+import hello.servlet.web.frontcontroller.MyView;
+import hello.servlet.web.frontcontroller.v2.controller.MemberFormControllerV2;
+import hello.servlet.web.frontcontroller.v2.controller.MemberListControllerV2;
+import hello.servlet.web.frontcontroller.v2.controller.MemberSaveControllerV2;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@WebServlet(name = "frontControllerServletV2", urlPatterns = "/front-controller/v2/*")
+public class FrontControllerServletV2 extends HttpServlet {
+
+    private Map<String, ControllerV2> controllerMap = new HashMap<>();
+
+    public FrontControllerServletV2() {
+        controllerMap.put("/front-controller/v2/members/new-form", new MemberFormControllerV2());
+        controllerMap.put("/front-controller/v2/members/save", new MemberSaveControllerV2());
+        controllerMap.put("/front-controller/v2/members", new MemberListControllerV2());
+    }
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("FrontControllerServletV2.service");
+
+        // /front-controller/v1/members
+        String requestURI = request.getRequestURI();
+
+        // 다형성을 사용!
+        ControllerV2 controllerV2 = controllerMap.get(requestURI);
+        if (controllerV2 == null) {
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+        MyView view = controllerV2.process(request, response);
+        view.render(request, response);
+    }
+}
