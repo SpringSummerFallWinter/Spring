687eb814a623d0f4e9333b77daffb06dd7c30789
sypark <soun997@gmail.com>
2023-06-02 00:34:59 +0900

    init: 프로젝트 세팅

---
+++ soyun/Spring-MVC-1/5.스프링MVC-구조이해/servlet/src/main/java/hello/servlet/web/frontcontroller/v1/ControllerV1.java
@@ -0,0 +1,13 @@
+package hello.servlet.web.frontcontroller.v1;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.rowset.serial.SerialException;
+import java.io.IOException;
+
+public interface ControllerV1 {
+
+    // 서블릿과 똑같은 모양
+    void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException;
+}
